c88fb1398ca0e4d7a39899c1e024c3436327b397
Yuzhe Sheng <ys766@cornell.edu>
2018-03-20 12:39:17 -0400

    Milestone 1

--- gallery.php
+++ gallery.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta name="author" content="Yuzhe Sheng" />
   <link rel="stylesheet" type="text/css" href="styles/all.css" media="all" />
-  <title>Home</title>
+  <title>Gallery</title>
 </head>
 
 <body>
--- init/init.sql
+++ init/init.sql
@@ -1,3 +1,43 @@
 /* TODO: create tables */
+CREATE TABLE `images` (
+  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+  `image_name` TEXT NOT NULL,
+  `description` TEXT,
+  `user_id` INTEGER NOT NULL
+);
 
+CREATE TABLE `tags` (
+  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+  `tag_name` TEXT NOT NULL
+);
+
+CREATE TABLE `image_tag` (
+  `image_id` INTEGER NOT NULL,
+  `tag_id` INTEGER NOT NULL
+);
+
+CREATE TABLE `accounts` (
+  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+  `username` TEXT NOT NULL UNIQUE,
+  `password` TEXT NOT NULL,
+  `session` TEXT,
+  `realname` TEXT, NOT NULL
+);
 /* TODO: initial seed data */
+INSERT INTO accounts (username, password, realname)
+VALUES ("ronald94@yahoo.com", "harrysheng0100", "Ronald Sheng");
+INSERT INTO accounts (username, password, realname)
+VALUES ("ys766", "I_love_Traveling", "Kevin G.");
+
+INSERT INTO images (image_name, description, user_id)
+VALUES ("Shanghai_1.jpg", "The Bund", 1);
+
+INSERT INTO tags (tag_name)
+VALUES ("Asian");
+INSERT INTO tags (tag_name)
+VALUES ("China");
+
+INSERT INTO image_tag(image_id, tag_id)
+VALUES (1,1);
+INSERT INTO image_tag(image_id, tag_id)
+VALUES (1,2);
