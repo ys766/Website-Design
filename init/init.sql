32e1c6027e89145af6d4fef984385b19a90cc247
Yuzhe Sheng <ys766@cornell.edu>
2018-03-25 17:46:28 -0400

    fix database table creation

--- init/init.sql
+++ init/init.sql
@@ -1,27 +1,28 @@
 /* TODO: create tables */
-CREATE TABLE `images` (
-  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-  `image_name` TEXT NOT NULL,
-  `description` TEXT,
-  `user_id` INTEGER NOT NULL
+CREATE TABLE images (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+  image_name TEXT NOT NULL,
+  description TEXT,
+  user_id INTEGER NOT NULL
 );
 
-CREATE TABLE `tags` (
-  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-  `tag_name` TEXT NOT NULL
+CREATE TABLE tags (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+  tag_name TEXT NOT NULL
 );
 
-CREATE TABLE `image_tag` (
-  `image_id` INTEGER NOT NULL,
-  `tag_id` INTEGER NOT NULL
+CREATE TABLE image_tag (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+  image_id INTEGER NOT NULL,
+  tag_id INTEGER NOT NULL
 );
 
-CREATE TABLE `accounts` (
-  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-  `username` TEXT NOT NULL UNIQUE,
-  `password` TEXT NOT NULL,
-  `session` TEXT,
-  `realname` TEXT, NOT NULL
+CREATE TABLE accounts (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+  username TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL,
+  session TEXT,
+  realname TEXT, NOT NULL
 );
 /* TODO: initial seed data */
 INSERT INTO accounts (username, password, realname)
