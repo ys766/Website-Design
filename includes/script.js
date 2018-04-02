8454ff22cd7d9d3093d7a072695148d05fa6bc6c
Yuzhe Sheng <ys766@cornell.edu>
2018-04-02 12:25:27 -0400

    Add javascript for gallery arrangement

--- includes/script.js
+++ includes/script.js
@@ -3,10 +3,7 @@ function galleryArrangement takes the image ids as an arrary and calculate the b
 images when the user wants to view all images at once. 
 The function galleryArrangment tries to position images in columns and tries to make each column each length
 */
-
-var heights = [];
-var widths = [];
-var colWidth = 500;
+var imageSizes = [];
 var min_w = Number.MAX_SAFE_INTEGER;
 var max_w = 0.0;
 var min_h = Number.MAX_SAFE_INTEGER;
@@ -16,13 +13,16 @@ var vertical = [];
 var horizontal = [];
 
 const IMGAGE_PATH = "/uploads/images/";
+const colWidth = 500;
 
 function createMidthHeight() {
 	// images is an array storing all the images in the Gallery
 	var images = document.getElementsByClassName("galleryImages");
 	images.forEach( function (image) {
-		heights.push(image.clientHeight);
-		widths.push(image.clientWidth);
+		imageSizes.push({
+			"width" : image.clientWidth,
+			"height" : image.clientHeight
+			})
 		if (image.clientWidth >= max_w) {
 			max_w = image.clientWidth;
 		}
@@ -36,16 +36,16 @@ function createMidthHeight() {
 			min_h = image.clientHeight;
 		}
 		if (image.clientHeight >= image.clientWidth * 1.8) {
-			vertical.push(
-				"width":image.clientWidth;
-				"height":image.clientHeight;
-				);
+			vertical.push({
+				"width":image.clientWidth,
+				"height":image.clientHeight
+				});
 		}
 		else {
-			horizontal.push(
-				"width":image.clientWidth;
-				"height":image.clientHeight;
-				)
+			horizontal.push({
+				"width":image.clientWidth,
+				"height":image.clientHeight
+				})
 		}
 	});
 }
@@ -60,11 +60,16 @@ of the images in the gallery.
 function arrangeImages(heights, widths) {
 	// the window to display the images are supposed to be scrollable 
 	// vertically but fixed in width according to different window sizes. 
+	var newMaxHeight = 0;
 	var window_width = document.getElementById("window").clientWidth;
 
 	// Scale the widths and heights according to the image grid
 	// set the width of all images to the same, and auto scale the height of 
 	// all images. 
-	heights = heights / widths * colWidth;
-}
+	imageSizes.forEach( function (size) {
+		size["height"] = size["height"] / size["width"] * colWidth;
+		if (size["height"] >= newMaxHeight) { newMaxHeight = size["height"]; }
+	});
+
+	
 }
